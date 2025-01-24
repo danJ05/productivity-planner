ebf7ea5378879745ef66eeee3f5f2031490cfb1c
danJ05 <miezandanieljr@gmail.com>
2025-01-24 16:37:24 +0000

    feat(auth):add register methods to save user on firebase server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 
 @Component({
@@ -9,6 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
 }
 
