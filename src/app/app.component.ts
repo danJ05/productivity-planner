3033400f917e9d83cc86b835d3fc348c699a4959
danJ05 <miezandanieljr@gmail.com>
2025-01-28 18:46:27 +0000

    Walking skeleton x firestore x database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {AuthenticationService} from './core/authentication.service';
+import {switchMap} from 'rxjs';
 
 
 @Component({
@@ -9,5 +11,20 @@ import {Component} from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  readonly #authenticationService = inject(AuthenticationService);
+
+  onLogin() {
+    const email = 'dev@gmail.com';
+    const password = '12345678';
+
+    this.#authenticationService.login(email,password).pipe(
+      switchMap(response =>{
+        console.log(response);
+        const {email,localId,idToken} = response;
+        return this.#authenticationService.save(email, localId,idToken)
+      })
+    ).subscribe(response => console.log(response))
+  }
 }
 
