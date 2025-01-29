daad9e28832af45bef41e040f89887c8e3945e5c
danJ05 <miezandanieljr@gmail.com>
2025-01-29 15:40:27 +0000

    chore(test): migrate from karma/jasmine to jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {AuthenticationService} from './core/authentication.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        AppComponent,
+        HttpClientModule // Ajout du HttpClientModule
+      ],
+      providers: [
+        AuthenticationService // Ajout du service d'authentification
+      ]
     }).compileComponents();
   });
 
