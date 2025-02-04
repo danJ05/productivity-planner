9243993328ed91fb3f7895e96eef381a6dc424a1
danJ05 <miezandanieljr@gmail.com>
2025-02-04 16:05:20 +0000

    test(home-banner):implement all test suite + add date-testid attributes

--- src/app/visitor/home/home.page.component.ts
+++ src/app/visitor/home/home.page.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import {HomeBannerDumbComponent} from './home-banner/home-banner.dumb.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    HomeBannerDumbComponent
+  ],
   templateUrl: './home.page.component.html',
   styleUrl: './home.page.component.scss'
 })
 export class HomePageComponent {
 
+  onBannerCliked() {
+    console.log('action when i click on button');
+  }
 }
